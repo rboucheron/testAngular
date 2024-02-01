c914acc192a58c0b22d5a600d4da48831cfa3542
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-02-01 13:53:11 +0100

    update

--- src/app/page-table-multi/page-table-multi.component.ts
+++ src/app/page-table-multi/page-table-multi.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-table-multi',
   templateUrl: './page-table-multi.component.html',
   styleUrl: './page-table-multi.component.css'
 })
+
 export class PageTableMultiComponent {
+@Input() n!: number; 
 
 }
